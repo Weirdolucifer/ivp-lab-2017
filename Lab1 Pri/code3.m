279b75fe9cf03382afdf8fb38dacd9faca177d23
Yash Sharma <yasharmaster@gmail.com>
2017-04-17 20:07:34 +0530

    first commit

---
+++ Lab1 Pri/code3.m
@@ -0,0 +1,17 @@
+panda=imread('panda.jpg');
+panda_int8=int8(panda);
+panda_uint8=uint8(panda);
+panda_gray=rgb2gray(panda);
+panda_single=im2single(panda);
+panda_double=im2double(panda);
+panda_int16=im2int16(panda);
+panda_uint16=im2uint16(panda);
+hold on;
+subplot(4,2,1), subimage(panda), title('normal');
+subplot(4,2,2), subimage(panda_int8), title('int8');
+subplot(4,2,3), subimage(panda_uint8), title('uint8');
+subplot(4,2,4), subimage(panda_gray), title('grayscale');
+subplot(4,2,5), subimage(panda_single), title('single');
+subplot(4,2,6), subimage(panda_double), title('double');
+subplot(4,2,7), subimage(panda_int16), title('int16');
+subplot(4,2,8), subimage(panda_uint16), title('uint16');
